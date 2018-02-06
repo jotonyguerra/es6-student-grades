684c32bb8cb846b3efac83f59a739f6960fe522c
Jotony Guerra <jotonyguerra@gmail.com>
2018-02-05 21:14:45 -0800

    first commit

---
+++ Student.js
@@ -0,0 +1,10 @@
+// define your student class, here
+
+// create a new student object
+let james = new Student("James", [80, 75.3, 90, 88, 70]);
+
+// print the average grade to the console
+console.log(james.averageGrade());
+
+// print the string representation of the student object
+console.log(james.toString());
