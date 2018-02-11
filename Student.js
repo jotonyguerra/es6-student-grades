aa69100d3f918095f5844f6f9a240998407b24f4
Jotony Guerra <jotonyguerra@gmail.com>
2018-02-10 19:13:00 -0800

    es6 working

--- Student.js
+++ Student.js
@@ -1,4 +1,22 @@
 // define your student class, here
+class Student {
+  constructor(name,grades) {
+    this.name = name;
+    this.grades = grades;
+  }
+
+  averageGrade() {
+    let sum = 0;
+    for(var i = 0; i < this.grades.length; i++) {
+      sum += this.grades[i];
+    }
+    return sum / this.grades.length;
+  }
+
+  toString() {
+    return `${this.name} has an average grade of ${this.averageGrade()}.`;
+  }
+}
 
 // create a new student object
 let james = new Student("James", [80, 75.3, 90, 88, 70]);
